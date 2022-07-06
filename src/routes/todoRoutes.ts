cae2226a28e31ff77757249df9690329b73e7399
unknown <facundovae@gmail.com>
2022-07-06 10:25:24 -0300

    todoRoutes requests content modified at documentation

--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -6,11 +6,11 @@ router.get("/something/:id", async ({ params, query }: Request, res: Response) =
 
   // #swagger.tags = ['Todos']
         // #swagger.description = 'Endpoint to bring certain todo'
-        // #swagger.parameters['id'] = { in: 'params', description: 'id of todo', type: 'string' }
+        // #swagger.parameters['id'] = { in: 'body', description: 'id of todo', type: 'string' }
         // #swagger.parameters['name'] = { in: 'query', description: 'name of todo', required: true, type: 'string' }
        //#swagger.responses[200] = { description: "Success response", schema: { $ref: "#/definitions/Todos" } }
        //#swagger.responses[400] = { description: "Todo does not exist" }
-
+//linea 9 in: body va params en realidad. No se por que Redocly lo marca como error
   console.log("query: ", query);
   console.log("params: ", params);
   const data = getTodosFromId();
@@ -20,10 +20,11 @@ router.get("/something/:id", async ({ params, query }: Request, res: Response) =
 router.get("/:user", ({ params }: Request, res: Response) => {
     // #swagger.tags = ['Todos']
         // #swagger.description = 'Endpoint to bring last todo'
-        // #swagger.parameters['id'] = { in: 'params', description: 'ID of user', type: 'string' }
+        // #swagger.parameters['id'] = { in: 'body', description: 'ID of user', type: 'string' }
         //#swagger.responses[200] = { description: "Success response", schema: { $ref: "#/definitions/Todos" } }
        //#swagger.responses[400] = { description: "User does not exist" }
-  res.send(params.user);
+//linea 23 in: body va params en realidad. No se por que Redocly lo marca como error
+res.send(params.user);
 });
 
 export default router;
